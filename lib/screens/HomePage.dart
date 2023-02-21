97b6e00e568482ac473d49605c1320bb61e59162
unknown <macoycoder@gmail.com>
2023-02-22 02:54:21 +0800

    [IMP] Added Banner and search bar

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_it/constants/colors.dart';
 import '../widgets/buildAppBar.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,8 +10,61 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: buildAppBar(),
-      body: Container(
-        child: const Text("Sample Text"),
+      body: ListView(
+        children: [
+          Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                width: (MediaQuery.of(context).size.width),
+                height: 135,
+                color: Colors.black,
+                child: Image.asset('assets/img/logo.png'),
+              ),
+              const Positioned(
+                bottom: -25,
+                left: 0,
+                right: 0,
+                child: Center(
+                  child: _SearchBar(),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _SearchBar extends StatelessWidget {
+  const _SearchBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 20),
+      elevation: 10,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: const TextField(
+          decoration: InputDecoration(
+            prefixIcon: Icon(
+              Icons.search,
+              color: primaryGray,
+            ),
+            border: InputBorder.none,
+            hintText: 'Search',
+            hintStyle: TextStyle(
+              color: primaryGray,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
       ),
     );
   }
