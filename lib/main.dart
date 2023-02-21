d47b9e94d7b7fe421c8665ed13053573c436ebb5
unknown <macoycoder@gmail.com>
2023-02-22 02:05:28 +0800

    [IMP] Added changing screen using BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shop_it/constants/colors.dart';
-import 'package:shop_it/widgets/buildAppBar.dart';
 
 import 'package:flutter/material.dart';
+import 'package:shop_it/screens/HomePage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -38,6 +38,14 @@ class MainApp extends StatefulWidget {
 class _MainApp extends State<MainApp> {
   int _selectedIndex = 0;
 
+  static const List<Widget> _screenOptions = <Widget>[
+    HomePage(),
+    Placeholder(),
+    HomePage(),
+    Placeholder(),
+    HomePage(),
+  ];
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -47,30 +55,38 @@ class _MainApp extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: buildAppBar(),
-      body: const Placeholder(),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset('assets/icons/nav_home.svg'),
-              label: "Home"),
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset('assets/icons/nav_shop.svg'),
-              label: "Shop"),
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset('assets/icons/nav_messages.svg'),
-              label: "Messages"),
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset('assets/icons/nav_fav.svg'),
-              label: "Favorites"),
-          BottomNavigationBarItem(
-              icon: SvgPicture.asset('assets/icons/nav_account.svg'),
-              label: "Account"),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: primaryOrange,
-        onTap: _onItemTapped,
-      ),
+      body: _screenOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    );
+  }
+
+  BottomNavigationBar _buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/nav_home.svg'),
+            activeIcon: SvgPicture.asset('assets/icons/nav_home_active.svg'),
+            label: "Home"),
+        BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/nav_shop.svg'),
+            activeIcon: SvgPicture.asset('assets/icons/nav_shop_active.svg'),
+            label: "Shop"),
+        BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/nav_messages.svg'),
+            label: "Messages"),
+        BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/nav_fav.svg'),
+            label: "Favorites"),
+        BottomNavigationBarItem(
+            icon: SvgPicture.asset('assets/icons/nav_account.svg'),
+            activeIcon: SvgPicture.asset('assets/icons/nav_account_active.svg'),
+            label: "Account"),
+      ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: primaryOrange,
+      selectedFontSize: 12,
+      onTap: _onItemTapped,
     );
   }
 }
